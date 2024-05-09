1b0a15b7fc73243767109424ae1d5a2e0b7b5e37
Isuru <isurumbandara@gmail.com>
2024-05-09 21:36:57 +0530

    image add and bottom nav add

--- screens/App.jsx
+++ screens/App.jsx
@@ -3,6 +3,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons'; // Import icons from Expo
 import HomeScreen from './HomeScreen';
 import SettingsScreen from './SettingsScreen';
+import FavoritesScreen from './FavoritesScreen'; // Import FavoritesScreen component
+import CartScreen from './CartScreen'; // Import CartScreen component
+import ProfileScreen from './ProfileScreen'; // Import ProfileScreen component
 
 const Tab = createBottomTabNavigator();
 
@@ -17,6 +20,12 @@ function App() {
             iconName = focused ? 'home' : 'home-outline';
           } else if (route.name === 'Settings') {
             iconName = focused ? 'settings' : 'settings-outline';
+          } else if (route.name === 'Favorites') {
+            iconName = focused ? 'heart' : 'heart-outline';
+          } else if (route.name === 'Cart') {
+            iconName = focused ? 'cart' : 'cart-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
           }
 
           // Return an Ionicons component with the appropriate name and style
@@ -25,6 +34,10 @@ function App() {
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Favorites" component={FavoritesScreen} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+      
+      <Tab.Screen name="Profile" component={ProfileScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
