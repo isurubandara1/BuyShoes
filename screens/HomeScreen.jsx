942a02a0ecee9dc17d57d04120b79a8fb37c8c27
Isuru <isurumbandara@gmail.com>
2024-05-10 15:57:07 +0530

    add map function to easy

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,62 +1,111 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Image1 from '../assets/images/adidas/a1.png';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+
+const products = [
+  {
+    image: require('../assets/images/adidas/a1.png'),
+    description: 'White and black leather adidas',
+    price: '100$'
+  },
+  {
+    image: require('../assets/images/adidas/a1.png'),
+    description: 'White and black leather adidas',
+    price: '120$'
+  },{
+    image: require('../assets/images/adidas/a1.png'),
+    description: 'White and black leather adidas',
+    price: '120$'
+  },{
+    image: require('../assets/images/adidas/a1.png'),
+    description: 'White and black leather adidas',
+    price: '120$'
+  },{
+    image: require('../assets/images/adidas/a1.png'),
+    description: 'White and black leather adidas',
+    price: '120$'
+  },{
+    image: require('../assets/images/adidas/a1.png'),
+    description: 'White and black leather adidas',
+    price: '120$'
+  },{
+    image: require('../assets/images/adidas/a1.png'),
+    description: 'White and black leather adidas',
+    price: '120$'
+  },{
+    image: require('../assets/images/adidas/a1.png'),
+    description: 'White and black leather adidas',
+    price: '120$'
+  },{
+    image: require('../assets/images/adidas/a1.png'),
+    description: 'White and black leather adidas',
+    price: '120$'
+  },{
+    image: require('../assets/images/adidas/a1.png'),
+    description: 'White and black leather adidas',
+    price: '120$'
+  },
+];
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
-      <View style={styles.imageContainer}>
-        <View style={styles.imageTextContainer}>
-          <Image source={Image1} style={styles.image} />
-          <Text style={styles.textDescription}>White and black lether adidas</Text>
-          <Text style={styles.textPrice}>100$</Text>
-        </View>
-        <View style={styles.imageTextContainer}>
-          <Image source={Image1} style={styles.image} />
-          <Text style={styles.textDescription}>White and black lether leass adidas</Text>
-          <Text style={styles.textPrice}>120$</Text>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <Text>HomeScreen</Text>
+        <View style={styles.imageContainer}>
+          {products.map((product, index) => (
+            <View style={styles.imageTextContainer} key={index}>
+              <Image source={product.image} style={styles.image} />
+              <Text style={styles.textDescription}>{product.description}</Text>
+              <Text style={styles.textPrice}>{product.price}</Text>
+            </View>
+          ))}
         </View>
       </View>
-    </View>
-  )
+    </ScrollView>
+  );
 }
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#fff', 
+    backgroundColor: '#fff',
   },
   imageContainer: {
     flexDirection: 'row',
-    marginVertical: 10, 
-  
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginVertical: 10,
   },
-  imageTextContainer:{
-    flexDirection:'column',
-    alignItems:'center',
-    justifyContent:"center",
-    height:120,
-    width:170,
-    padding:10,
+  imageTextContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 200,
+    width: '45%', 
+    margin: 5,
+    padding: 10,
+    backgroundColor: '#F5DAD2',
+    borderRadius: 10,
   },
   image: {
-    width: 150,
+    width: '100%',
     height: 100,
     resizeMode: 'contain',
-    paddingRight:10, 
+    marginBottom: 10,
   },
-  textDescription:{
-    fontSize:15,
-    textAlign:'center',
+  textDescription: {
+    fontSize: 15,
+    textAlign: 'center',
   },
-  textPrice:{
-    fontSize:20,
-    textAlign:'center',
-    fontWeight:'900',
+  textPrice: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: '900',
+    marginTop: 5,
   },
-  
 });
