43c3bb59987722672e6aa90783bd3d5c58dd06b7
Isuru <isurumbandara@gmail.com>
2024-05-15 23:58:48 +0530

    page create

--- screens/LargePage.jsx
+++ screens/LargePage.jsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const LargePage = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>LargePage</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
