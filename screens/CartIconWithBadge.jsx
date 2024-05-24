354b05c5e9a0ba283bd2eeef58b354de5b41ee5d
isurubandara1 <isurumbandara@gmail.com>
2024-05-24 23:58:03 +0530

    small changes

--- screens/CartIconWithBadge.jsx
+++ screens/CartIconWithBadge.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { CartContext } from './CartContext'; // Adjust the path as needed
 
@@ -7,27 +7,39 @@ const CartIconWithBadge = ({ name, color, size }) => {
   const { cartItemCount } = useContext(CartContext);
 
   return (
-    <View style={{ width: 24, height: 24, margin: 5 }}>
+    <View style={styles.iconContainer}>
       <Ionicons name={name} size={size} color={color} />
       {cartItemCount > 0 && (
-        <View
-          style={{
-            position: 'absolute',
-            right: -6,
-            top: -3,
-            backgroundColor: 'red',
-            borderRadius: 6,
-            width: 12,
-            height: 12,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>{cartItemCount}</Text>
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeText}>{cartItemCount}</Text>
         </View>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    width: 24,
+    height: 24,
+    margin: 5,
+  },
+  badgeContainer: {
+    position: 'absolute',
+    right: -8,
+    top: -12,
+    backgroundColor: 'red',
+    borderRadius: 6,
+    width: 20,
+    height: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  badgeText: {
+    color: 'white',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+});
 
 export default CartIconWithBadge;
