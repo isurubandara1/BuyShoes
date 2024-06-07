91f2d310626b60f40a15409e8fdb3f46ae1d73d0
isurubandara1 <isurumbandara@gmail.com>
2024-06-08 02:57:12 +0530

    backed connect

--- Backend/demo/src/main/java/com/example/demo/model/Order.java
+++ Backend/demo/src/main/java/com/example/demo/model/Order.java
@@ -1,5 +1,9 @@
 package com.example.demo.model;
 
+import java.util.List;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,10 +21,26 @@ import lombok.NoArgsConstructor;
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // details
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fullName;
     private String address;
     private String town;
     private Long telephoneNumber;
+
+    @ElementCollection
+    private List<OrderItem> items;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Embeddable
+    public static class OrderItem {
+
+        private double price;
+        private String description;
+        private String size;
+        private int count;
+        private double total;
+    }
 }
